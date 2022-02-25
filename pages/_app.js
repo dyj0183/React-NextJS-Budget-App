3ff79e5d498cbee64a3462d7d558cdf4f43d2379
Tree <thefloatingtree@gmail.com>
2022-02-25 14:09:56 -0700

    add chakra-ui, jotai, and react-hook-form

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.css'
 
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({
+
+})
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
