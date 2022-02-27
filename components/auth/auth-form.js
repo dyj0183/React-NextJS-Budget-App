46d4f08a43035e7fc87f39a9fe5672dd08dd646e
Yu-Chun Tung <dyj0183@byui.edu>
2022-02-26 20:51:33 -0700

    created basic sign up and login form with css

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,8 +1,12 @@
-import classess from "./auth-form.module.css";
+import classes from "./auth-form.module.css";
+import { useState } from "react";
 
 const AuthForm = () => {
+	const [chooseLogin, setChooseLogin] = useState(true);
+
 	return (
-		<div>
+		<div className={classes.card}>
+			<h1>{chooseLogin ? "Login" : "Sign Up"}</h1>
 			<form>
 				<div>
 					<label htmlFor="email">Email</label>
@@ -13,6 +17,10 @@ const AuthForm = () => {
 					<label htmlFor="password">Password</label>
 					<br />
 					<input type="text" id="password" name="password" />
+				</div>
+				<div>
+					<button className={classes.authButton}>{chooseLogin ? 'Login' : 'Create Account'}</button>
+                    <button type="button" className={classes.toggleButton}>{chooseLogin ? 'Create new account' : 'Login with existing account'}</button>
 				</div>
 			</form>
 		</div>
