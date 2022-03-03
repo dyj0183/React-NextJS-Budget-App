d997662e75448b5eaedd3ae2c0e36bd7e2d964ef
Seth Bolander <bol15015@byui.edu>
2022-03-02 22:42:42 -0700

    moved everything to summary/index

---
+++ pages/summary/index.js
@@ -0,0 +1,61 @@
+import {
+  Flex,
+  Stack,
+  Box,
+  Table,
+  TableCaption,
+  Thead,
+  Tbody,
+  Th,
+  Tr,
+  // Td,
+  StatGroup,
+  Stat,
+  StatLabel,
+  StatNumber,
+} from "@chakra-ui/react";
+import { useMediaQuery } from "@chakra-ui/react";
+
+export default function Summary() {
+  const [isSmallScreen] = useMediaQuery("(max-width: 1280px)");
+
+  return (
+    <Flex justify="space-around">
+      <Box>
+        <Table>
+          <TableCaption placement="top">Expenses</TableCaption>
+          <Thead>
+            <Tr>
+              <Th>Name</Th>
+              <Th>Frequency</Th>
+              <Th>Amount</Th>
+              <Th>Annual Amount</Th>
+            </Tr>
+          </Thead>
+          <Tbody></Tbody>
+        </Table>
+      </Box>
+      <Stack
+        direction={isSmallScreen ? "column" : "row"}
+        borderWidth="1px"
+        borderRadius="lg"
+        padding="1rem"
+        spacing="1rem"
+      >
+        <StatGroup borderWidth="1px" borderRadius="lg" padding="1rem">
+          <Stat>
+            <StatLabel>Total Income</StatLabel>
+            <StatNumber>$100</StatNumber>
+          </Stat>
+          {/* Generate stats for individual incomes */}
+        </StatGroup>
+        <StatGroup borderWidth="1px" borderRadius="lg" padding="1rem">
+          <Stat>
+            <StatLabel>Remaining Balance</StatLabel>
+            <StatNumber>${/* Display remaining */}</StatNumber>
+          </Stat>
+        </StatGroup>
+      </Stack>
+    </Flex>
+  );
+}
