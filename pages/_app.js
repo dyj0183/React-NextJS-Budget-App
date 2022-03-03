69ffe1eb07255c81c70fe0a38e0b24c0bc0d6bd2
Seth Bolander <bol15015@byui.edu>
2022-03-02 20:33:09 -0700

    added unstyled page header

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,31 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
-import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme, HStack } from "@chakra-ui/react";
 import { Provider } from "next-auth/client";
+import Link from "next/link";
 
-const theme = extendTheme({
-
-})
+const theme = extendTheme({});
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
       <Provider session={pageProps.session}>
+        <HStack
+          justify="space-between"
+          borderBottom="1px solid black"
+          h="4rem"
+          padding="1rem"
+        >
+          <Link href="/">MY LOGO</Link>
+          <HStack spacing="2rem">
+            <Link href="/add-income-and-expense">Edit Budget</Link>
+            <Link href="/auth">Login/Logout</Link>
+          </HStack>
+        </HStack>
         <Component {...pageProps} />
       </Provider>
     </ChakraProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
