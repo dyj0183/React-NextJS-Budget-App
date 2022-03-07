1db31c65710045417711f8d60581337cdb0b02c3
Yu-Chun Tung <dyj0183@byui.edu>
2022-03-07 14:11:49 -0700

    built backend authentication logic for login

--- helper/bcrypt.js
+++ helper/bcrypt.js
@@ -5,3 +5,9 @@ export const hashPassword = async (password) => {
 	const hashedPassword = await hash(password, 12);
 	return hashedPassword;
 };
+
+// Compare the user login password with the hashedPassword from the database
+export const verifyPassword = async (password, hashedPassword) => {
+	const isValid = await compare(password, hashedPassword);
+	return isValid;
+};
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,9 +1,40 @@
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
+import { verifyPassword } from "../../../helper/bcrypt";
+import { connectToMongoDB } from "../../../helper/mongodb";
 
 export default NextAuth({
 	session: {
 		jwt: true,
 	},
-	providers: [],
+	providers: [
+		Providers.Credentials({
+			async authorize(credentials) {
+				const mongoClient = await connectToMongoDB();
+
+				const usersCollection = mongoClient.db().collection("users");
+
+				const user = await usersCollection.findOne({
+					email: credentials.email,
+				});
+
+				if (!user) {
+					mongoClient.close();
+					throw new Error("No user found");
+				}
+
+				// Verify the password
+				const isValid = verifyPassword(credentials.password, user.password);
+
+				if (!isValid) {
+					mongoClient.close();
+					throw new Error("Password doesn't match. Please try again");
+				}
+
+				mongoClient.close();
+				// Return to let next auth know that it succeed
+				return { email: user.email };
+			},
+		}),
+	],
 });
