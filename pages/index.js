fd6c4f4d8f9cdb7df5121a1da47b4ebc18452a98
Yu-Chun Tung <dyj0183@byui.edu>
2022-03-09 08:52:21 -0700

    used session and Jotai to set up isLoggedIn logic and get userId

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,23 @@
 import { Fragment, useState } from "react";
 import AuthForm from "../components/auth/auth-form";
 import Summary from "../components/summary/Summary";
+import { useSession } from "next-auth/client";
+import { useAtom } from "jotai";
+import { userIdAtom } from "../store/atom";
 
 export default function Home() {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   // TODO: set userId with session
-  const userId = "621d742436c291324f692b38";
+  const [session, loading] = useSession();
+  const [userId] = useAtom(userIdAtom);
+
+  // Set up !isLoggedIn to prevent infinite loop
+  if (session && !loading && !isLoggedIn) {
+    setIsLoggedIn(true);
+
+    console.log("home page user id");
+    console.log(userId);
+  }
 
   return (
     <Fragment>
