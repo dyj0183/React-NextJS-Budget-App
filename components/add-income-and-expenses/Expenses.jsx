c2bc99f4ce69f723e4f85fdbc533ee2c545f6db1
Seth Bolander <bol15015@byui.edu>
2022-03-09 11:19:54 -0700

    a few api changes and error display issues

--- components/add-income-and-expenses/Expenses.jsx
+++ components/add-income-and-expenses/Expenses.jsx
@@ -1,66 +1,110 @@
-import { Button, HStack, Input, Text, VStack, Select, Box } from "@chakra-ui/react";
+import {
+  Button,
+  HStack,
+  Input,
+  Text,
+  VStack,
+  Select,
+  Box,
+} from "@chakra-ui/react";
 import { useState } from "react";
 import { Controller, useFieldArray, useForm } from "react-hook-form";
 import ReactSelect from "react-select/creatable";
 
 export default function Expenses({ onFinish, onBack }) {
+  const [categories, setCategories] = useState([]);
 
-    const [categories, setCategories] = useState([])
+  const { register, control, watch } = useForm({
+    defaultValues: {
+      expenses: [
+        { name: "", amount: "", frequency: "Weekly", category: "Category" },
+      ],
+    },
+  });
 
-    const { register, control, watch } = useForm({
-        defaultValues: {
-            expenses: [{ name: "", amount: "", frequency: "Weekly", category: "Category" }]
-        }
-    })
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "expenses",
+  });
 
-    const {
-        fields,
-        append,
-        remove
-    } = useFieldArray({
-        control,
-        name: "expenses"
-    })
+  const fieldValues = watch();
 
-    const fieldValues = watch()
-
-    return (
-        <VStack w={'100%'} align={'start'}>
-            <Text fontSize={'4xl'} fontWeight={'bold'}>Add Your Expenses</Text>
-            {fields.map((item, index) => {
-                return (
-                    <HStack w={'100%'} key={item.id}>
-                        <Input flexGrow={'1'} placeholder="Name" {...register(`expenses.${index}.name`)} />
-                        <Input type={'number'} step={'0.01'} min={"0.01"} placeholder="Amount" {...register(`expenses.${index}.amount`)} />
-                        <Select {...register(`expenses.${index}.frequency`)} >
-                            <option value='Weekly'>Weekly</option>
-                            <option value='Bi-Monthly'>Bi-Monthly</option>
-                            <option value='Monthly'>Monthly</option>
-                            <option value='Bi-Annually'>Bi-Annually</option>
-                            <option value='Annually'>Annually</option>
-                        </Select>
-                        <Box flexBasis={'100%'}>
-                            <Controller control={control} name={`expenses.${index}.category`} render={({ field: { onChange, onBlur, value } }) => (
-                                <ReactSelect
-                                    onCreateOption={(category) => setCategories(prev => [...prev, { label: category, value: category }])}
-                                    onChange={onChange}
-                                    onBlur={onBlur}
-                                    value={value}
-                                    options={categories}
-                                />
-                            )}
-                            />
-
-                        </Box>
-                        <Button w={'15rem'} colorScheme={'red'} onClick={() => remove(index)}>Remove</Button>
-                    </HStack>
-                )
-            })}
-            <Button onClick={() => append({ name: "", amount: "", frequency: "Weekly" })} isFullWidth>Add an Expense</Button>
-            <HStack w={'100%'} justify={'space-between'}>
-                <Button isFullWidth onClick={onBack}>Back</Button>
-                <Button isFullWidth colorScheme={'blue'} onClick={() => onFinish(fieldValues)}>Finish</Button>
-            </HStack>
-        </VStack>
-    )
+  return (
+    <VStack w={"100%"} align={"start"}>
+      <Text fontSize={"4xl"} fontWeight={"bold"}>
+        Add Your Expenses
+      </Text>
+      {fields.map((item, index) => {
+        return (
+          <HStack w={"100%"} key={item.id}>
+            <Input
+              flexGrow={"1"}
+              placeholder="Name"
+              {...register(`expenses.${index}.name`)}
+            />
+            <Input
+              type={"number"}
+              step={"0.01"}
+              min={"0.01"}
+              placeholder="Amount"
+              {...register(`expenses.${index}.amount`)}
+            />
+            <Select {...register(`expenses.${index}.frequency`)}>
+              <option value="weekly">Weekly</option>
+              <option value="biweekly">Bi-Weekly</option>
+              <option value="bimonthly">Bi-Monthly</option>
+              <option value="monthly">Monthly</option>
+              <option value="biannually">Bi-Annually</option>
+              <option value="annually">Annually</option>
+            </Select>
+            <Box flexBasis={"100%"}>
+              <Controller
+                control={control}
+                name={`expenses.${index}.category`}
+                render={({ field: { onChange, onBlur, value } }) => (
+                  <ReactSelect
+                    onCreateOption={(category) =>
+                      setCategories((prev) => [
+                        ...prev,
+                        { label: category, value: category },
+                      ])
+                    }
+                    onChange={onChange}
+                    onBlur={onBlur}
+                    value={value}
+                    options={categories}
+                  />
+                )}
+              />
+            </Box>
+            <Button
+              w={"15rem"}
+              colorScheme={"red"}
+              onClick={() => remove(index)}
+            >
+              Remove
+            </Button>
+          </HStack>
+        );
+      })}
+      <Button
+        onClick={() => append({ name: "", amount: "", frequency: "Weekly" })}
+        isFullWidth
+      >
+        Add an Expense
+      </Button>
+      <HStack w={"100%"} justify={"space-between"}>
+        <Button isFullWidth onClick={onBack}>
+          Back
+        </Button>
+        <Button
+          isFullWidth
+          colorScheme={"blue"}
+          onClick={() => onFinish(fieldValues)}
+        >
+          Finish
+        </Button>
+      </HStack>
+    </VStack>
+  );
 }
--- pages/api/summary/[id].js
+++ pages/api/summary/[id].js
@@ -3,28 +3,35 @@ import { connectToMongoDB } from "../../../helper/mongodb";
 export default async function summaryHandler(req, res) {
   var userId = req.query.id;
   if (userId === undefined) {
-    res.status(401);
+    console.log("User id not set. Try logging in?");
+    res.status(401).end();
     return;
   }
 
+  // get mongo connection
+  const mongoClient = await connectToMongoDB();
   try {
-    // get mongo connection
-    const mongoClient = await connectToMongoDB();
     // connect to database
     const db = mongoClient.db();
 
     const summary = await db.collection("budgets").findOne({ userId: userId });
-    if (summary === null) {
-      res.status(404);
+    if (
+      summary === null ||
+      summary.incomes.length === 0 ||
+      summary.expenses.length === 0
+    ) {
+      res.status(404).end();
       mongoClient.close();
       return;
     }
+    console.log(summary);
     res.status(200).json({
       incomes: summary.incomes,
       expenses: summary.expenses,
     });
-    mongoClient.close();
   } catch (err) {
-    res.status(500);
+    res.status(500).json(err);
+  } finally {
+    mongoClient.close();
   }
 }
